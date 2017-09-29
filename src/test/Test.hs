6286ce413902fc8c46daec76d148a39d86fda257
nikivazou <nikivazou@gmail.com>
2017-09-28 23:57:45 -0400

    set travis to run liquid in labels

--- src/test/Test.hs
+++ src/test/Test.hs
@@ -1,6 +1,22 @@
 module Main where
 
 import System.Exit
+import System.Process
 
 main :: IO ()
-main = exitFailure
+main = do 
+  c1 <- runCommand' ("cd " ++ sourceDir)
+  runLiquid "Labels.hs"
+
+runLiquid :: String -> IO ExitCode
+runLiquid fname = runCommand' ("stack exec -- liquid " ++ fname)
+
+sourceDir :: String
+sourceDir = "src/src"
+
+runCommand' :: String -> IO ExitCode
+runCommand' str = 
+  do ps <- runCommand str -- (str ++ "> log 2>&1")
+     ec <- waitForProcess ps 
+     putStrLn ("\nCommand `" ++ str ++ "` exited with " ++ show ec)
+     return ec
