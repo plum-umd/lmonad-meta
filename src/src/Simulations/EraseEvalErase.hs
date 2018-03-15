8900c7074763bf68c2321bf1bf4976bb1c3d699f
James Parker <dev@jamesparker.me>
2018-03-15 03:16:26 -0400

    uncomment all cases for eraseevalerase

--- src/src/Simulations/EraseEvalErase.hs
+++ src/src/Simulations/EraseEvalErase.hs
@@ -20,44 +20,44 @@ import ProofCombinators
  / [size t]
  @-}
 eraseEvalEraseSimulation :: Label -> Term -> Proof
--- eraseEvalEraseSimulation l t@(TIf TTrue t2 t3) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TIf (εTerm l TTrue) (εTerm l t2) (εTerm l t3)))
---     ==. εTerm l (eval (TIf TTrue (εTerm l t2) (εTerm l t3)))
---     ==. εTerm l (εTerm l t2) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. εTerm l t2 ? εTermIdempotent l t2
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TIf TFalse t2 t3) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TIf (εTerm l TFalse) (εTerm l t2) (εTerm l t3)))
---     ==. εTerm l (eval (TIf TFalse (εTerm l t2) (εTerm l t3)))
---     ==. εTerm l (εTerm l t3) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. εTerm l t3 ? εTermIdempotent l t3
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TIf t1 t2 t3) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TIf (εTerm l t1) (εTerm l t2) (εTerm l t3)))
---     ==. εTerm l (TIf (eval (εTerm l t1)) (εTerm l t2) (εTerm l t3)) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TIf (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2)) (εTerm l (εTerm l t3))
---     ==. TIf (εTerm l (eval (εTerm l t1))) (εTerm l t2) (εTerm l t3) ? εTermIdempotent l t2 &&& εTermIdempotent l t3
---     ==. TIf (εTerm l (eval t1)) (εTerm l t2) (εTerm l t3) ? eraseEvalEraseSimulation l t1
---     ==. εTerm l (eval t)
---     *** QED
+eraseEvalEraseSimulation l t@(TIf TTrue t2 t3) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TIf (εTerm l TTrue) (εTerm l t2) (εTerm l t3)))
+    ==. εTerm l (eval (TIf TTrue (εTerm l t2) (εTerm l t3)))
+    ==. εTerm l (εTerm l t2) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. εTerm l t2 ? εTermIdempotent l t2
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TIf TFalse t2 t3) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TIf (εTerm l TFalse) (εTerm l t2) (εTerm l t3)))
+    ==. εTerm l (eval (TIf TFalse (εTerm l t2) (εTerm l t3)))
+    ==. εTerm l (εTerm l t3) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. εTerm l t3 ? εTermIdempotent l t3
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TIf t1 t2 t3) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TIf (εTerm l t1) (εTerm l t2) (εTerm l t3)))
+    ==. εTerm l (TIf (eval (εTerm l t1)) (εTerm l t2) (εTerm l t3)) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TIf (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2)) (εTerm l (εTerm l t3))
+    ==. TIf (εTerm l (eval (εTerm l t1))) (εTerm l t2) (εTerm l t3) ? εTermIdempotent l t2 &&& εTermIdempotent l t3
+    ==. TIf (εTerm l (eval t1)) (εTerm l t2) (εTerm l t3) ? eraseEvalEraseSimulation l t1
+    ==. εTerm l (eval t)
+    *** QED
 
 eraseEvalEraseSimulation l t@(TFix (TLam x t1)) = 
         εTerm l (eval (εTerm l t))
-    ==! εTerm l (eval (TFix (εTerm l (TLam x t1))))
-    ==! εTerm l (eval (TFix (TLam x (εTerm l t1))))
-    ==: εTerm l (subst (Sub x (TFix (TLam x (εTerm l t1)))) (εTerm l t1)) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
-    ==! εTerm l (subst (Sub x (TFix (εTerm l (TLam x t1)))) (εTerm l t1))
-    ==! εTerm l (subst (Sub x (εTerm l (TFix (TLam x t1)))) (εTerm l t1))
-    ==: εTerm l (subst (Sub x (TFix (TLam x t1))) t1)
+    ==. εTerm l (eval (TFix (εTerm l (TLam x t1))))
+    ==. εTerm l (eval (TFix (TLam x (εTerm l t1))))
+    ==. εTerm l (subst (Sub x (TFix (TLam x (εTerm l t1)))) (εTerm l t1)) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. εTerm l (subst (Sub x (TFix (εTerm l (TLam x t1)))) (εTerm l t1))
+    ==. εTerm l (subst (Sub x (εTerm l (TFix (TLam x t1)))) (εTerm l t1))
+    ==. εTerm l (subst (Sub x (TFix (TLam x t1))) t1)
         ? eraseSubErase l x (TFix (TLam x t1)) t1
-    ==! εTerm l (eval t)
+    ==. εTerm l (eval t)
     *** QED
 
 eraseEvalEraseSimulation l t@(TFix t1) = 
@@ -71,113 +71,113 @@ eraseEvalEraseSimulation l t@(TFix t1) =
 
 eraseEvalEraseSimulation l t@(TApp (TLam x t1) t2) = 
         εTerm l (eval (εTerm l t))
-    ==! εTerm l (eval (TApp (εTerm l (TLam x t1)) (εTerm l t2)))
-    ==! εTerm l (eval (TApp (TLam x (εTerm l t1)) (εTerm l t2)))
-    ==: εTerm l (subst (Sub x (εTerm l t2)) (εTerm l t1))
+    ==. εTerm l (eval (TApp (εTerm l (TLam x t1)) (εTerm l t2)))
+    ==. εTerm l (eval (TApp (TLam x (εTerm l t1)) (εTerm l t2)))
+    ==. εTerm l (subst (Sub x (εTerm l t2)) (εTerm l t1))
         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
-    ==: εTerm l (subst (Sub x t2) t1)
+    ==. εTerm l (subst (Sub x t2) t1)
         ? eraseSubErase l x t2 t1
-    ==! εTerm l (eval t)
+    ==. εTerm l (eval t)
     *** QED
 
 eraseEvalEraseSimulation l t@(TApp t1 t2) = 
         εTerm l (eval (εTerm l t))
-    ==! εTerm l (eval (TApp (εTerm l t1) (εTerm l t2)))
-    ==: εTerm l (TApp (eval (εTerm l t1)) (εTerm l t2)) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
-    ==! TApp (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
-    ==: TApp (εTerm l (eval t1)) (εTerm l (εTerm l t2)) ? eraseEvalEraseSimulation l t1 &&& εTermIdempotent l t2
-    ==! εTerm l (eval t)
+    ==. εTerm l (eval (TApp (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (TApp (eval (εTerm l t1)) (εTerm l t2)) ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TApp (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
+    ==. TApp (εTerm l (eval t1)) (εTerm l (εTerm l t2)) ? eraseEvalEraseSimulation l t1 &&& εTermIdempotent l t2
+    ==. εTerm l (eval t)
     *** QED
 
--- eraseEvalEraseSimulation l t@(TJoin t1@(TVLabel l1) t2@(TVLabel l2)) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TJoin (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (eval (TJoin t1 t2))
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TJoin t1@(TVLabel l1) t2) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TJoin (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (eval (TJoin t1 (εTerm l t2)))
---     ==. εTerm l (TJoin t1 (eval (εTerm l t2))) 
---         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TJoin (εTerm l t1) (εTerm l (eval (εTerm l t2)))
---     ==. TJoin t1 (εTerm l (eval t2)) ? eraseEvalEraseSimulation l t2
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TJoin t1 t2) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TJoin (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (TJoin (eval (εTerm l t1)) (εTerm l t2))
---         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TJoin (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
---     ==. TJoin (εTerm l (eval t1)) (εTerm l t2)
---         ? eraseEvalEraseSimulation l t1 
---         &&& εTermIdempotent l t2
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TMeet t1@(TVLabel l1) t2@(TVLabel l2)) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TMeet (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (eval (TMeet t1 t2))
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TMeet t1@(TVLabel l1) t2) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TMeet (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (eval (TMeet t1 (εTerm l t2)))
---     ==. εTerm l (TMeet t1 (eval (εTerm l t2))) 
---         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TMeet (εTerm l t1) (εTerm l (eval (εTerm l t2)))
---     ==. TMeet t1 (εTerm l (eval t2)) ? eraseEvalEraseSimulation l t2
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TMeet t1 t2) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TMeet (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (TMeet (eval (εTerm l t1)) (εTerm l t2))
---         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TMeet (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
---     ==. TMeet (εTerm l (eval t1)) (εTerm l t2)
---         ? eraseEvalEraseSimulation l t1 
---         &&& εTermIdempotent l t2
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TCanFlowTo t1@(TVLabel l1) t2@(TVLabel l2)) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TCanFlowTo (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (eval (TCanFlowTo t1 t2))
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TCanFlowTo t1@(TVLabel l1) t2) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TCanFlowTo (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (eval (TCanFlowTo t1 (εTerm l t2)))
---     ==. εTerm l (TCanFlowTo t1 (eval (εTerm l t2))) 
---         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TCanFlowTo (εTerm l t1) (εTerm l (eval (εTerm l t2)))
---     ==. TCanFlowTo t1 (εTerm l (eval t2)) ? eraseEvalEraseSimulation l t2
---     ==. εTerm l (eval t)
---     *** QED
--- 
--- eraseEvalEraseSimulation l t@(TCanFlowTo t1 t2) = 
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TCanFlowTo (εTerm l t1) (εTerm l t2)))
---     ==. εTerm l (TCanFlowTo (eval (εTerm l t1)) (εTerm l t2))
---         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TCanFlowTo (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
---     ==. TCanFlowTo (εTerm l (eval t1)) (εTerm l t2)
---         ? eraseEvalEraseSimulation l t1 
---         &&& εTermIdempotent l t2
---     ==. εTerm l (eval t)
---     *** QED
+eraseEvalEraseSimulation l t@(TJoin t1@(TVLabel l1) t2@(TVLabel l2)) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TJoin (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (eval (TJoin t1 t2))
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TJoin t1@(TVLabel l1) t2) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TJoin (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (eval (TJoin t1 (εTerm l t2)))
+    ==. εTerm l (TJoin t1 (eval (εTerm l t2))) 
+        ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TJoin (εTerm l t1) (εTerm l (eval (εTerm l t2)))
+    ==. TJoin t1 (εTerm l (eval t2)) ? eraseEvalEraseSimulation l t2
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TJoin t1 t2) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TJoin (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (TJoin (eval (εTerm l t1)) (εTerm l t2))
+        ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TJoin (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
+    ==. TJoin (εTerm l (eval t1)) (εTerm l t2)
+        ? eraseEvalEraseSimulation l t1 
+        &&& εTermIdempotent l t2
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TMeet t1@(TVLabel l1) t2@(TVLabel l2)) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TMeet (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (eval (TMeet t1 t2))
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TMeet t1@(TVLabel l1) t2) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TMeet (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (eval (TMeet t1 (εTerm l t2)))
+    ==. εTerm l (TMeet t1 (eval (εTerm l t2))) 
+        ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TMeet (εTerm l t1) (εTerm l (eval (εTerm l t2)))
+    ==. TMeet t1 (εTerm l (eval t2)) ? eraseEvalEraseSimulation l t2
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TMeet t1 t2) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TMeet (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (TMeet (eval (εTerm l t1)) (εTerm l t2))
+        ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TMeet (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
+    ==. TMeet (εTerm l (eval t1)) (εTerm l t2)
+        ? eraseEvalEraseSimulation l t1 
+        &&& εTermIdempotent l t2
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TCanFlowTo t1@(TVLabel l1) t2@(TVLabel l2)) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TCanFlowTo (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (eval (TCanFlowTo t1 t2))
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TCanFlowTo t1@(TVLabel l1) t2) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TCanFlowTo (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (eval (TCanFlowTo t1 (εTerm l t2)))
+    ==. εTerm l (TCanFlowTo t1 (eval (εTerm l t2))) 
+        ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TCanFlowTo (εTerm l t1) (εTerm l (eval (εTerm l t2)))
+    ==. TCanFlowTo t1 (εTerm l (eval t2)) ? eraseEvalEraseSimulation l t2
+    ==. εTerm l (eval t)
+    *** QED
+
+eraseEvalEraseSimulation l t@(TCanFlowTo t1 t2) = 
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TCanFlowTo (εTerm l t1) (εTerm l t2)))
+    ==. εTerm l (TCanFlowTo (eval (εTerm l t1)) (εTerm l t2))
+        ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TCanFlowTo (εTerm l (eval (εTerm l t1))) (εTerm l (εTerm l t2))
+    ==. TCanFlowTo (εTerm l (eval t1)) (εTerm l t2)
+        ? eraseEvalEraseSimulation l t1 
+        &&& εTermIdempotent l t2
+    ==. εTerm l (eval t)
+    *** QED
 
 eraseEvalEraseSimulation l t@(TUnlabel t1) = 
         εTerm l (eval (εTerm l t))
@@ -213,35 +213,35 @@ eraseEvalEraseSimulation l t@(TLabel t1 t2) =
     ==. εTerm l (eval t)
     *** QED
 
--- eraseEvalEraseSimulation l t@(TLabelOf (TLabeledTCB l' t1)) =
---     if  l' `canFlowTo` l then
---             εTerm l (eval (εTerm l t))
---         ==. εTerm l (eval (TLabelOf (εTerm l (TLabeledTCB l' t1))))
---         ==. εTerm l (eval (TLabelOf (TLabeledTCB l' (εTerm l t1))))
---         ==. εTerm l (TVLabel l')
---             ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---         ==. εTerm l (eval t)
---         *** QED
---         
---     else
---             εTerm l (eval (εTerm l t))
---         ==! εTerm l (eval (TLabelOf (εTerm l (TLabeledTCB l' t1))))
---         ==! εTerm l (eval (TLabelOf (TLabeledTCB l' THole)))
---         ==: εTerm l (TVLabel l')
---             ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---         ==! εTerm l (eval t)
---         *** QED
--- 
--- eraseEvalEraseSimulation l t@(TLabelOf t1) =
---         εTerm l (eval (εTerm l t))
---     ==. εTerm l (eval (TLabelOf (εTerm l t1)))
---     ==. εTerm l (TLabelOf (eval (εTerm l t1)))
---         ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
---     ==. TLabelOf (εTerm l (eval (εTerm l t1)))
---     ==. TLabelOf (εTerm l (eval t1))
---         ? eraseEvalEraseSimulation l t1
---     ==. εTerm l (eval t)
---     *** QED
+eraseEvalEraseSimulation l t@(TLabelOf (TLabeledTCB l' t1)) =
+    if  l' `canFlowTo` l then
+            εTerm l (eval (εTerm l t))
+        ==. εTerm l (eval (TLabelOf (εTerm l (TLabeledTCB l' t1))))
+        ==. εTerm l (eval (TLabelOf (TLabeledTCB l' (εTerm l t1))))
+        ==. εTerm l (TVLabel l')
+            ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+        ==. εTerm l (eval t)
+        *** QED
+        
+    else
+            εTerm l (eval (εTerm l t))
+        ==. εTerm l (eval (TLabelOf (εTerm l (TLabeledTCB l' t1))))
+        ==. εTerm l (eval (TLabelOf (TLabeledTCB l' THole)))
+        ==. εTerm l (TVLabel l')
+            ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+        ==. εTerm l (eval t)
+        *** QED
+
+eraseEvalEraseSimulation l t@(TLabelOf t1) =
+        εTerm l (eval (εTerm l t))
+    ==. εTerm l (eval (TLabelOf (εTerm l t1)))
+    ==. εTerm l (TLabelOf (eval (εTerm l t1)))
+        ? propagateExceptionFalseEvalsToNonexception t &&& erasePropagateExceptionFalse l t
+    ==. TLabelOf (εTerm l (eval (εTerm l t1)))
+    ==. TLabelOf (εTerm l (eval t1))
+        ? eraseEvalEraseSimulation l t1
+    ==. εTerm l (eval t)
+    *** QED
 
 eraseEvalEraseSimulation l t@(TToLabeled t1@(TVLabel l') t2) =
         εTerm l (eval (εTerm l t))
@@ -267,9 +267,9 @@ eraseEvalEraseSimulation l t@(TToLabeled t1 t2) =
     ==. εTerm l (eval t)
     *** QED
 
-eraseEvalEraseSimulation l t@TException = 
+eraseEvalEraseSimulation l t = 
         εTerm l (eval (εTerm l t))
     ==. εTerm l (eval t)
     *** QED
-eraseEvalEraseSimulation l t = undefined
+-- eraseEvalEraseSimulation l t = undefined
 
