e8b8eb645bb3acbcfeb4caa6f0d3d2dcf8b58481
James Parker <dev@jamesparker.me>
2018-04-09 16:47:35 -0400

    Try to move simulationsTBind to Simulations.hs

--- src/Simulations.hs
+++ src/Simulations.hs
@@ -12,7 +12,6 @@ import Simulations.Language
 import Simulations.MetaFunctions
 import Simulations.Programs
 import Simulations.Helpers
-import Simulations.TBind
 import Simulations.EraseEvalErase
 import Termination
 
@@ -266,6 +265,8 @@ simulations'' p@(Pg lc c m t@(TCanFlowTo _ _)) l = case propagateException t of
 
 simulations'' p@(Pg lc c m t@(TBind t1 t2)) l =
         evalEraseProgram (ε l p) l 
+    ==. evalEraseProgram (Pg lc c m (εTerm l t)) l
+    ==. evalEraseProgram (Pg lc c m (TBind (εTerm l t1) (εTerm l t2))) l
     ==. ε l (evalProgram p) ? simulationsTBind l lc c m t1 t2 
     *** QED 
 
@@ -808,4 +809,65 @@ evalFlow = undefined
 evalFlowErase :: Label -> Program -> Proof
 {-@ evalFlowErase :: l:Label -> p:Program -> {canFlowTo (pLabel p) l => canFlowTo (pLabel (evalProgram (ε l p))) l} @-}
 evalFlowErase = undefined 
+
+{-@ simulationsTBind :: l:Label -> lc:{Label | canFlowTo lc l} -> c:Label -> m:Memory -> t1:Term -> t2:{Term | terminates (Pg lc c m (TBind t1 t2)) } 
+ -> {v : Proof | ε l (evalProgram (Pg lc c m (TBind (εTerm l t1) (εTerm l t2)))) == ε l (evalProgram (Pg lc c m (TBind t1 t2))) }
+ / [evalSteps (Pg lc c m (TBind t1 t2)), 1]
+ @-}
+
+simulationsTBind :: Label -> Label -> Label -> Memory -> Term -> Term -> Proof 
+
+simulationsTBind l lc c m t1 t2 
+  | l'' `canFlowTo` l, l' `canFlowTo` l 
+  =   ε l (evalProgram (Pg lc c m (TBind (εTerm l t1) (εTerm l t2))))
+  ==. ε l (Pg l' c' m' (TApp (εTerm l t2) t')) 
+  ==. Pg l' c' m' (εTerm l (TApp (εTerm l t2) t')) 
+  ==. Pg l' c' m' (TApp (εTerm l (εTerm l t2)) (εTerm l t')) 
+  ==. Pg l' c' m' (TApp (εTerm l t2) (εTerm l t')) ? εTermIdempotent l t2  
+  ==. Pg l' c' m' (TApp (εTerm l t2) (εTerm l t')) ? 
+        (     Pg l' c' m' (εTerm l t')
+          ==. ε l (Pg l' c' m' t')
+          ==. ε l (evalProgramStar (Pg lc c m (εTerm l t1)))
+          ==. ε l (evalProgramStar (ε l (Pg lc c m t1)))
+          ==. ε l (evalProgramStar (Pg lc c m t1)) 
+              ? terminationAxiomTBind lc c m t1 t2 &&& simulationsStar'' (Pg lc c m t1) l 
+          ==. ε l (Pg l'' c'' m'' t'')
+          ==. Pg l'' c'' m'' (εTerm l t'')
+          *** QED 
+        )
+  ==. Pg l'' c'' m'' (TApp (εTerm l t2) (εTerm l t''))
+  ==. Pg l'' c'' m'' (εTerm l (TApp t2 t''))
+  ==. ε l (Pg l'' c'' m'' (TApp t2 t''))
+  ==. ε l (evalProgram (Pg lc c m (TBind t1 t2)))
+  *** QED 
+  | not (l'' `canFlowTo` l), not (l' `canFlowTo` l) 
+  =   ε l (evalProgram (Pg lc c m (TBind (εTerm l t1) (εTerm l t2))))
+  ==. ε l (Pg l' c' m' (TApp (εTerm l t2) t')) 
+  ==. PgHole 
+  ==. ε l (Pg l'' c'' m'' (TApp t2 t''))
+  ==. ε l (evalProgram (Pg lc c m (TBind t1 t2)))
+  *** QED 
+  | not (l'' `canFlowTo` l),  l' `canFlowTo` l
+  =   Pg l' c' m' (εTerm l t')
+  ==. ε l (Pg l' c' m' t')
+  ==. ε l (evalProgramStar (Pg lc c m (εTerm l t1)))
+  ==. ε l (evalProgramStar (ε l (Pg lc c m t1)))
+  ==. ε l (evalProgramStar (Pg lc c m t1)) 
+      ? terminationAxiomTBind lc c m t1 t2 &&& simulationsStar'' (Pg lc c m t1) l 
+  ==. ε l (Pg l'' c'' m'' t'')
+  ==. PgHole
+  *** QED 
+  | l'' `canFlowTo` l, not (l' `canFlowTo` l)
+  =   PgHole
+  ==. ε l (Pg l' c' m' t')
+  ==. ε l (evalProgramStar (Pg lc c m (εTerm l t1)))
+  ==. ε l (evalProgramStar (ε l (Pg lc c m t1)))
+  ==. ε l (evalProgramStar (Pg lc c m t1)) 
+      ? terminationAxiomTBind lc c m t1 t2 &&& simulationsStar'' (Pg lc c m t1) l 
+  ==. ε l (Pg l'' c'' m'' t'')
+  ==. Pg l'' c'' m'' (εTerm l t'')
+  *** QED 
+   where
+    Pg l' c' m' t'     = evalProgramStar (Pg lc c m (εTerm l t1))
+    Pg l'' c'' m'' t'' = evalProgramStar (Pg lc c m t1)
 
