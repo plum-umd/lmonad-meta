d7e9263435c78025549c86fc67ecdd86a4010545
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2019-02-15 18:04:19 -0500

    add update nothing just files to test/Update.hs

--- tests/Update.hs
+++ tests/Update.hs
@@ -8,11 +8,15 @@ main :: IO ()
 main = liquid [
     "EraseTableAny.hs"
   , "LabelUpdateCheck.hs"
+  , "LabelUpdateCheckNothingJust.hs"
   , "Simulations/UpdateAny.hs"
+  , "Simulations/UpdateAnyNothingJust.hs"
   , "Simulations/UpdateRowBySMT.hs"
   , "Simulations/UpdateOne.hs"
+  , "Simulations/UpdateOneNothingJust.hs"
   , "Simulations/Update.hs"
+  , "Simulations/UpdateNothingJust.hs"
   , "Simulations/TUpdateFound.hs"
-  , "Simulations/UpdateAnyNothingJust.hs"
+  , "Simulations/TUpdateFoundNothingJust.hs"
   , "Simulations/TUpdate.hs"
   ]
